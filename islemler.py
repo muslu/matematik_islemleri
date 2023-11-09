47381e0d8b89b8faf7c6bdbcc09d5a6220a3b7ba
Muslu YÜKSEKTEPE <musluyuksektepe@gmail.com>
2023-11-09 11:36:52 +0300

    vergi hesaplamalar eklendi

--- islemler.py
+++ islemler.py
@@ -93,17 +93,33 @@ class m:
         except ValueError:
             return "Hata: Geçersiz değer karekök alma işlemi yapılamaz."
 
+    def vergi_dahil_fiyat(self, vergi_dahil_fiyat, vergi_orani, yuvarlama=2):
+        vergi_haric_fiyat = round(vergi_dahil_fiyat / (1 + (vergi_orani / 100)), yuvarlama)
+        vergi_miktari = round(vergi_dahil_fiyat - vergi_haric_fiyat, yuvarlama)
+        toplam_fiyat = round(vergi_haric_fiyat + vergi_miktari, yuvarlama)
+        return f'vergi_haric_fiyat: {vergi_haric_fiyat}, vergi_miktari: {vergi_miktari}, toplam_fiyat: {toplam_fiyat}'
+
+    def vergi_haric_fiyat(self, vergi_haric_fiyat, vergi_orani, yuvarlama=2):
+        vergi_miktari = round(vergi_haric_fiyat * (vergi_orani / 100), yuvarlama)
+        toplam_fiyat = round(vergi_haric_fiyat + vergi_miktari, yuvarlama)
+        return f'vergi_haric_fiyat: {vergi_haric_fiyat}, vergi_miktari: {vergi_miktari}, toplam_fiyat: {toplam_fiyat}'
+
 
 # Kullanım örneği
+print(m(100).vergi_dahil_fiyat(vergi_dahil_fiyat=100, vergi_orani=20, yuvarlama=2))
+print(m(100).vergi_haric_fiyat(vergi_haric_fiyat=100, vergi_orani=20, yuvarlama=2))
+print(m(100).vergi_dahil_fiyat(100, 20))
+print(m(100).vergi_haric_fiyat(100, 20))
+
 print(m(20).toplama(10, detay=True))
-print(m(20).cikarma(10))
-print(m(20).carpma(10))
-print(m(20).bolme(10))
-print(m(2).us_alma(3))
-print(m(20).mod_alma(7, detay=True))
-print(m(5).faktoriyel())
-print(m(10).permutasyon(3, detay=True))
-print(m(10).kombinasyon(3))
-print(m(100).logaritma(10))
-print(m(30).trigonometrik_fonksiyonlar())
-print(m(16).karekok_alma())
+print(m(20).cikarma(10, True))
+# print(m(20).carpma(10))
+# print(m(20).bolme(10))
+# print(m(2).us_alma(3))
+# print(m(20).mod_alma(7, detay=True))
+# print(m(5).faktoriyel())
+# print(m(10).permutasyon(3, detay=True))
+# print(m(10).kombinasyon(3))
+# print(m(100).logaritma(10))
+# print(m(30).trigonometrik_fonksiyonlar())
+# print(m(16).karekok_alma())
